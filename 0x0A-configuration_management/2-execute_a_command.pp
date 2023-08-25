37d63470ec398f8d2743d1d80475b6f005d80f79
Adooz <kndonake@gmail.com>
2023-08-25 07:25:27 -0700

    manifest to kill a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # kills a process named killmenow.
 exec { 'kill_killmenow' :
-  command => 'pkil killmenow',
-  path    => ['/usr/bin'],
-  onlyif  => 'pgrep killmenow',
+  command     => 'pkil killmenow',
+  path        => ['/usr/bin'],
+  onlyif      => 'pgrep killmenow',
+  refreshonly => true,
 }
