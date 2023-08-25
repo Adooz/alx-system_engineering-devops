10abbbebc40d3154414bb15056588096af650e24
Adooz <kndonake@gmail.com>
2023-08-25 07:40:58 -0700

    changed command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # kills a process named killmenow.
 exec { 'kill_killmenow' :
-  command     => 'pkill -f killmenow',
+  command     => 'pkill -SIGTERM killmenow',
   path        => '/usr/bin',
   refreshonly => true,
 }
