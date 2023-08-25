6d995f32e329a08f8192d3804a9b80b420a8787f
Adooz <kndonake@gmail.com>
2023-08-25 16:17:33 -0700

    fixed puppet-linter error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,26 +1,18 @@
-# make changes to config file using puppet
-# Class: ssh_config
+# Puppet Manifest: configure_ssh.pp
 #
-# This class manages SSH client configuration using the Augeas tool.
-# It ensures specific settings in the SSH configuration file.
+# This Puppet manifest uses the Augeas resource to configure SSH settings in the ssh_config file.
+# It disables PasswordAuthentication and sets IdentityFile for a specific match condition.
 #
-# Parameters:
-#   None
+# Note: This manifest assumes that the Augeas lens 'Ssh.lns' is available.
 #
-# Actions:
-#   - Sets PasswordAuthentication to 'no'
-#   - Sets IdentityFile to '~/.ssh/school'
 #
-# Example usage:
-#   include ssh_config
-class ssh_config {
-  augeas { 'configure_ssh':
-    incl    => '/etc/ssh/ssh_config',  # Path to the SSH configuration file
-    lens    => 'Ssh.lns',              # Augeas lens for SSH configuration
-    changes => [
-      "set Match[1]/Settings/PasswordAuthentication no",    # Disable password authentication
-      "set Match[1]/Settings/IdentityFile ~/.ssh/school",    # Set IdentityFile to ~/.ssh/school
-    ],
-  }
-}
 
+# Define the Augeas resource to configure SSH
+augeas { 'configure_ssh':
+  incl    => '/etc/ssh/ssh_config',  # Path to the SSH configuration file
+  lens    => 'Ssh.lns',              # Augeas lens for SSH configuration
+  changes => [
+    'set Match[1]/Settings/PasswordAuthentication no',    # Disable password authentication
+    'set Match[1]/Settings/IdentityFile ~/.ssh/school',    # Set IdentityFile to ~/.ssh/school
+  ],
+}
