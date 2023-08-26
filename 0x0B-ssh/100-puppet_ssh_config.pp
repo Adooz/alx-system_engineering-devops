6555908effb4a7c1572bcb2cc69466ab2260e25a
Adooz <kndonake@gmail.com>
2023-08-26 00:09:16 +0000

    changed manifest code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,15 @@
-# Puppet Manifest: configure_ssh.pp
-#
-# This Puppet manifest uses the Augeas resource to configure SSH settings in the ssh_config file.
-# It disables PasswordAuthentication and sets IdentityFile for a specific match condition.
-#
-# Note: This manifest assumes that the Augeas lens 'Ssh.lns' is available.
-#
-#
+# make changes to config file using Puppet
 
-# Define the Augeas resource to configure SSH
-augeas { 'configure_ssh':
-  incl    => '/etc/ssh/ssh_config',  # Path to the SSH configuration file
-  lens    => 'Ssh.lns',              # Augeas lens for SSH configuration
-  changes => [
-    'set Match[1]/Settings/PasswordAuthentication no',    # Disable password authentication
-    'set Match[1]/Settings/IdentityFile ~/.ssh/school',    # Set IdentityFile to ~/.ssh/school
-  ],
+include stdlib
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
 }
